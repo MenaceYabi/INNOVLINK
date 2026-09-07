02d9823e56ee5e46c104014de6c1ad59393f0fac
Biyaga Yohann <biyagayohann37@gmail.com>
2026-09-07 21:56:33 +0100

    Correction finale du stockage statique et du wsgi config

--- config/settings.py
+++ config/settings.py
@@ -179,6 +179,6 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
